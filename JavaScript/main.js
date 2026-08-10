4361ba3dd3107422b2ad239fe34d98db7253c824
BentancorLautaro <lautaroaugustobentancor@gmail.com>
2026-08-10 11:51:59 -0300

    Delete JavaScript directory

--- JavaScript/main.js
+++
@@ -1,8 +0,0 @@
-function initializeApp() {
-  const audioManager = new AudioManager();
-  const handleStartGame = () => console.log('Iniciando juego...');
-
-  new MainMenu(audioManager, handleStartGame);
-}
-
-document.addEventListener('DOMContentLoaded', initializeApp);
